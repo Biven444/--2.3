978e52edc06f668d0eb232b7bb9bf38ef5b3f4ae
Biven444 <biven2010@gmail.com>
2024-10-23 15:38:51 +0300

    Update module2.3.py

--- module2.3.py
+++ module2.3.py
@@ -5,11 +5,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 my_list.pop(4)
 a = 0
-while len (my_list) > a :
-    if my_list [a] > a :
+while len (my_list) > 0 :
+    if my_list [a] > 0 :
         print(my_list[a])
         a+=1
-    if my_list [a] < a:
+    if my_list [a] < 0:
         print()
         break
 
